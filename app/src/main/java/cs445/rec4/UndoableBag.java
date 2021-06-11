2cbf8f0ae986ecbdbb97a6e9ce66402cb2f945a9
Anzu <Anzu@zu-mac.local>
2021-06-11 11:20:23 -0400

    Finished rec4

--- app/src/main/java/cs445/rec4/UndoableBag.java
+++ app/src/main/java/cs445/rec4/UndoableBag.java
@@ -9,15 +9,23 @@ import java.lang.UnsupportedOperationException;
  * @author William C. Garrison III
  */
 
+ //extends ArrayBag<E> 
+ //can call super() call the ArrayBag methods 
+
 public class UndoableBag<E> extends ArrayBag<E> {
     // TODO Create two stack reference variables called undoStack and redoStack
+
+    StackInterface<Action<E>> undoStack; //Interface will be reference variable 
+    StackInterface<Action<E>> redoStack; //want to store Action objects into the stacks
+        //the Action class is also generic 
+        //Action can store any generic type 
 
     /**
      * Creates an empty bag with default capacity.
      */
     public UndoableBag() {
         // Call ArrayBag Default Constructor
-        super();
+        super(); 
 
         undoStack = new LinkedStack<Action<E>>();
         redoStack = new LinkedStack<Action<E>>();
@@ -35,7 +43,16 @@ public class UndoableBag<E> extends ArrayBag<E> {
 
         // TODO keep track of added entry for undo operations
 
-        return result;
+        redoStack.clear(); //get rid of the redo - only wanna redo the most recent action
+        //can get confusing if you don't clear the redoStack 
+
+        Action<E> inserted = new Action<E>('i', newEntry);
+        //must be Action<E> because Action is generic 
+
+        undoStack.push( inserted ); 
+        //is added to top of stack 
+
+        return result; //true or false 
     }
 
     /**
@@ -50,6 +67,12 @@ public class UndoableBag<E> extends ArrayBag<E> {
 
         // TODO keep track of the item removed for later undo operations
 
+        if( removedItem != null ) //why check ????
+        {
+            redoStack.clear(); 
+            Action<E> removed = new Action<E>('r', removedItem); 
+            undoStack.push( removed ); 
+        }
         return removedItem;
     }
 
@@ -60,10 +83,21 @@ public class UndoableBag<E> extends ArrayBag<E> {
      */
     @Override
     public boolean remove(E anEntry) {
+        if( anEntry == null)
+        {
+            return false; 
+        }
         // Call ArrayBag remove method
         boolean result = super.remove(anEntry);
+        
 
         // TODO keep track of the item removed for later undo operations
+
+        if (result) {
+        redoStack.clear();
+        Action<E> removed = new Action<E>('r', anEntry);
+        undoStack.push(removed);
+        }
 
         return result;
     }
@@ -85,7 +119,26 @@ public class UndoableBag<E> extends ArrayBag<E> {
     public boolean undo() {
         // TODO implement the undo method (including fixing the return value)
 
-        return false;
+        if( undoStack.isEmpty()){
+            return false; }
+
+        Action<E> operation = undoStack.pop(); 
+        //what if stack is empty? 
+        //would throw an EmptyStackException
+        redoStack.push(operation); 
+
+        switch( operation.getAction())
+        {
+            case 'i':
+                return super.remove( operation.getData()); 
+                
+            case 'r': 
+                return super.add( operation.getData() ); 
+
+            default: //if none were triggered 
+                return false; 
+        }
+
     }
 
     /**
@@ -95,7 +148,27 @@ public class UndoableBag<E> extends ArrayBag<E> {
     public boolean redo() {
         // TODO implement the redo method (including fixing the return value)
 
-        return false;
+        if( redoStack.isEmpty()){
+            return false; }
+
+        Action<E> operation = redoStack.pop(); 
+        //what if stack is empty? 
+        //would throw an EmptyStackException
+
+        undoStack.push(operation); 
+
+        switch( operation.getAction())
+        {
+            case 'i':
+                return super.add( operation.getData()); 
+                
+            case 'r': 
+                return super.remove( operation.getData() ); 
+
+            default: //if none were triggered 
+                return false; 
+        }
+
     }
 
 }
